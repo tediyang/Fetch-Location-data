609987aae4b539487ce07a85f077821e2d338f96
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2021-08-30 17:27:45 +0100

    Add initial project files

---
+++ app/message.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def retrieve_ip_address():
+    '''Return IP address of the computer'''
+    response = requests.get('https://api.ipify.org')
+
+    return response.text
+
+
+def get_geolocation(ip_address):
+    '''Get geolocation of an ip address'''
+    response = requests.get(f'https://ipinfo.io/{ip_address}')
+    data = response.json()
+    city, country = data['city'], data['country']
+    coords = [float(coord) for coord in data['loc'].split(',')]
+
+    return city, country, coords
+
+
+def greet():
+    ip_address = retrieve_ip_address()
+    information = get_geolocation(ip_address)
+    
+
+    return f'Hello, you are presently in {information[0]},{information[1]} \
+and your cordinates are latitude: {information[2][0]} and longitude: {information[2][1]}'
+
+if __name__ == '__main__':
+    print(greet())
+
